8d6e1d64f1b576b74c080409f8f425778b3dc19f
Guibertagna <guirogeriobertagna00@gmail.com>
2023-08-23 15:16:15 -0300

    implementação select

--- src/app/services/filme.service.ts
+++ src/app/services/filme.service.ts
@@ -8,8 +8,8 @@ export class FilmeService {
   catalogoFilme : Filme [] = [];
 
   constructor() { 
-    let c1: Filme = new Filme("PINTO GORDO", "adada", 223232,"sasasasa", 1212121)
-    let c2: Filme = new Filme("PINTO GORDO", "adada", 223232,"sasasasa", 1212121)
+    let c1: Filme = new Filme("Como eu era antes de você", "romace", 1998,"Bom", 150)
+    let c2: Filme = new Filme("Se beber não case", "comédia", 2000,"Mediano", 125)
     this.catalogoFilme.push(c1);
     this.catalogoFilme.push(c2);
   }
